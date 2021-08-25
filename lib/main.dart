09a7bdf8530198217eab530448b371c271d288ce
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-08-25 06:08:52 -0400

    data passing

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String title = "Greetings App";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      title: title,
+      home: MyHomePage(
+        title: title,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -24,15 +31,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     name = "swarna";
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     String x = "swarna";
-    return Scaffold( appBar: AppBar(
-
-    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Column(children: [
         Text("hello $name"),
         TextButton(
